facc0934d07c5e5270bf7158b8a71f0a37c78993
Patrick Seitz <seitzpr@gmail.com>
2024-09-24 17:41:20 -0400

    Updated Hash Table

--- include/libpds.h
+++ include/libpds.h
@@ -1,6 +1,10 @@
 #ifndef LIBPDS_H
 #define LIBPDS_H
 
+#include <stdint.h>
+
+#define MAX_NAME 256
+
 typedef struct Stack
 {
     char *stack;
@@ -23,5 +27,19 @@ typedef struct HashMap
 {
     /* data */
 } HashMap;
+
+/* Structs for testing */
+typedef struct Person
+{
+    char name[MAX_NAME];
+    int age;
+} Person;
+
+Person **hash_init();                                                // Create hash table
+int hash();                                                          // Generate hash
+int hash_insert(Person **hash_table, char *name, int age, int hash); // Insert value into hash table
+int hash_delete(Person **hash_table, int idx);                       // Delete value from hash table
+int hash_get();                                                      // Get value from hash table
+void hash_print_table(Person **hash_table, int table_size);          // Print the whole hash table
 
 #endif
--- src/libpds.c
+++ src/libpds.c
@@ -1,6 +1,83 @@
+#include <libpds.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+#define TABLE_SIZE 10
+
 int main()
 {
-    printf("Hello World\n");
+    Person **ptr_array = hash_init();
+
+    hash_insert(ptr_array, "Jeff", 120, 4);
+    hash_insert(ptr_array, "Rick", 54, 0);
+
+    hash_print_table(ptr_array, TABLE_SIZE);
+
+    hash_get(ptr_array, 0);
+    hash_delete(ptr_array, 0);
+
+    hash_print_table(ptr_array, TABLE_SIZE);
+
+    hash_get(ptr_array, 4);
+    hash_get(ptr_array, 0);
 
     return 0;
+}
+
+Person **hash_init()
+{
+    Person **arr = malloc(sizeof(Person) * 10);
+
+    for (int i = 0; i < TABLE_SIZE; i++)
+    {
+        Person *ptr = malloc(sizeof(Person));
+        arr[i] = ptr;
+        // arr[i] = malloc(sizeof(Person) * 10);
+        snprintf(arr[i]->name, 256, "%s", "NULL");
+        arr[i]->age = 0;
+    }
+
+    return arr;
+}
+
+int hash()
+{
+    return 5;
+}
+
+int hash_insert(Person **hash_table, char *name, int age, int hash)
+{
+    int key = hash;
+    int idx = key % TABLE_SIZE;
+    strcpy(hash_table[idx]->name, name);
+    hash_table[idx]->age = age;
+
+    return 0;
+}
+
+int hash_delete(Person **hash_table, int key)
+{
+    int idx = key % TABLE_SIZE;
+    strcpy(hash_table[idx]->name, "NULL");
+    hash_table[idx]->age = 0;
+
+    return 0;
+}
+
+int hash_get(Person **hash_table, int key)
+{
+    int idx = key % TABLE_SIZE;
+    printf("%d: Name: %s Age: %d\n", idx, hash_table[idx]->name, hash_table[idx]->age);
+
+    return 0;
+}
+
+void hash_print_table(Person **hash_table, int table_size)
+{
+    for (int i = 0; i < table_size; i++)
+    {
+        printf("%d Name: %s Age: %d\n", i, hash_table[i]->name, hash_table[i]->age);
+        printf("--------------------\n");
+    }
 }
